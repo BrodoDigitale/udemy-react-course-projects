07c3b8730279af8d9128f5e91b4966ea84562a44
Iuliia Sensi <sensi.julia@gmail.com>
2023-08-08 14:24:57 +0500

    feat: add Header component

---
+++ 07-food-order-app/src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImg from "../../assets/meals.jpg";
+
+export const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="a table full of food" />
+      </div>
+    </>
+  );
+};
