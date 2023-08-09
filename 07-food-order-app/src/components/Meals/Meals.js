f0333c74e30a9e42319d3679f57115ad3dcb4628
Iuliia Sensi <sensi.julia@gmail.com>
2023-08-09 15:06:25 +0500

    feat: add Meals components

---
+++ 07-food-order-app/src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { MealsSummary } from "./MealsSummary";
+import { AvailiableMeals } from "./AvailiableMeals";
+
+export const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailiableMeals />
+    </>
+  );
+};
