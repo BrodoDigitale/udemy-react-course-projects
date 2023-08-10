7d2a4ea00cb994d77048c2c57f94d7ad430c5931
Iuliia Sensi <sensi.julia@gmail.com>
2023-08-11 00:17:04 +0500

    feat: add cartButton animation using useEffect

--- 07-food-order-app/src/components/Layout/HeaderCartButton.js
+++ 07-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import { CartContext } from "../../store/cart-context";
 
 export const HeaderCartButton = ({ onClick, totalItems }) => {
+  const [btnIsAnimated, setBtnIsAnimated] = useState(false);
+  const ctx = useContext(CartContext);
+  const { items } = ctx;
+
+  const buttonClasses = `${classes.button} ${
+    btnIsAnimated ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length > 0) {
+      setBtnIsAnimated(true);
+    }
+
+    const timer = setTimeout(() => {
+      setBtnIsAnimated(false);
+    }, 300);
+
+    //cleanup function
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={buttonClasses} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
