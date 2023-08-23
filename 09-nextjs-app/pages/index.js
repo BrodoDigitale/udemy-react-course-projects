aa41908e5d753fa312b20c3b58bb55f296eeca40
Iuliia Sensi <sensi.julia@gmail.com>
2023-08-23 23:31:23 +0500

    add http project structure

--- 09-nextjs-app/pages/index.js
+++
@@ -1,6 +0,0 @@
-//route for fomepage
-
-export default function Page() {
-    return <h1>Homepage!</h1>
-    
-}
--- 09-nextjs-app/pages/news.js
+++
@@ -1,7 +0,0 @@
-//route for /news
-
-export const NewsPage = () => {
-  return <h1>News</h1>;
-};
-
-export default NewsPage;
