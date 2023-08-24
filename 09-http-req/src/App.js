8a1e503e8ef2c7608f4dc75951b659b46a121934
Iuliia Sensi <sensi.julia@gmail.com>
2023-08-24 11:29:10 +0500

    send request from useEffect

--- 09-http-req/src/App.js
+++ 09-http-req/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -7,7 +7,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [fetchError, setFetchError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     try {
       setIsLoading(true);
       const res = await fetch("https://swapi.dev/api/films");
@@ -30,7 +30,11 @@ function App() {
       setFetchError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>No movies found</p>;
 
