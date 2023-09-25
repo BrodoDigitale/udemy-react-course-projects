5c077af1625597712a68464a375fd9cca449c39c
Iuliia Sensi <sensi.julia@gmail.com>
2023-09-25 22:42:57 +0200

    add refs to check user input

--- 07-food-order-app/src/components/Cart/CheckoutForm.js
+++ 07-food-order-app/src/components/Cart/CheckoutForm.js
@@ -1,27 +1,41 @@
 import classes from "./Checkout.module.css";
+import { useRef } from "react";
 
 const CheckoutForm = props => {
 
+const nameInput = useRef();
+const streetInput = useRef();
+const capInput = useRef();
+const cityInput = useRef();
+
+
 const onConfirm = (evt) => {
     evt.preventDefault();
+    const name = nameInput.current.value;
+    const street = streetInput.current.value;
+    const cap = capInput.current.value;
+    const city = cityInput.current.value;
+
+    //validation
+    console.log(name, street, city, cap)
 }
 return (
   <form onSubmit={onConfirm} className={classes.form}>
     <div className={classes.control}>
       <label htmlFor="name">Your name</label>
-      <input type="text" id="name" />
+      <input ref={nameInput} type="text" id="name" />
     </div>
     <div className={classes.control}>
       <label htmlFor="street">Street</label>
-      <input type="text" id="street" />
+      <input ref={streetInput} type="text" id="street" />
     </div>
     <div className={classes.control}>
       <label htmlFor="CAP">Postal code</label>
-      <input type="text" id="CAP" />
+      <input ref={capInput} type="text" id="CAP" />
     </div>
     <div className={classes.control}>
       <label htmlFor="city">City</label>
-      <input type="text" id="city" />
+      <input ref={cityInput} type="text" id="city" />
     </div>
     <div className={classes.actions}>
       <button>Confirm</button>
