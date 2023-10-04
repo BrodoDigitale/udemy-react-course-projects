0f88f73bb32dd57776a89894848c444e678b3c7d
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-04 08:45:32 +0200

    access store inside App component with useSelector

--- 14-async-redux/src/App.js
+++ 14-async-redux/src/App.js
@@ -2,10 +2,15 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+
 function App() {
+
+  const showCart = useSelector(state => state.ui.cartIsOpen);
+
   return (
     <Layout>
-      <Cart />
+      {showCart &&<Cart />}
       <Products />
     </Layout>
   );
