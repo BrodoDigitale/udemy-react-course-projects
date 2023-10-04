643858a17b1a1cfea84215253bc8df68bcc9aee9
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-04 08:39:49 +0200

    dispatch toggle action for showing cart

--- 14-async-redux/src/components/Cart/CartButton.js
+++ 14-async-redux/src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
 import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+import { useDispatch } from 'react-redux';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartToggleHandler = () => {
+    dispatch(uiActions.toggleCartIsOpen());
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={cartToggleHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
