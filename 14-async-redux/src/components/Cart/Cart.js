685b5d44f8ed089ef55ac912f24ea5c5f58b283c
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-05 07:41:16 +0200

    show cart items inside the cart with useSelector

--- 14-async-redux/src/components/Cart/Cart.js
+++ 14-async-redux/src/components/Cart/Cart.js
@@ -1,15 +1,27 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
 const Cart = (props) => {
+  const items = useSelector((state) => state.cart.items);
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {items.map((i) => (
+          <li key={i.id}>
+            <CartItem
+              item={{
+                id: i.id,
+                title: i.name,
+                quantity: i.quantity,
+                total: i.totalPrice,
+                price: i.price,
+              }}
+            />
+          </li>
+        ))}
       </ul>
     </Card>
   );
