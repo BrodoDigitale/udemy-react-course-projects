4edab67d599067fc2d6ff8e6f63576ef78718a0f
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-05 07:42:02 +0200

    show number of items in the cart on the button

--- 14-async-redux/src/components/Cart/CartButton.js
+++ 14-async-redux/src/components/Cart/CartButton.js
@@ -1,16 +1,18 @@
 import classes from './CartButton.module.css';
 import { uiActions } from '../../store/ui-slice';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const totalItems = useSelector(state => state.cart.totalQuantity);
   const cartToggleHandler = () => {
     dispatch(uiActions.toggleCartIsOpen());
   }
   return (
     <button className={classes.button} onClick={cartToggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalItems}</span>
     </button>
   );
 };
