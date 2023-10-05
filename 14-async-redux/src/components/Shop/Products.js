c49f3a079336ed96225eabcf233c939ec1482efe
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-05 07:44:19 +0200

    add dummy data for products

--- 14-async-redux/src/components/Shop/Products.js
+++ 14-async-redux/src/components/Shop/Products.js
@@ -1,16 +1,25 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  { id: 1, name: "book", price: 6, description: "lorem ipsum" },
+  { id: 2, name: "book2", price: 3, description: "lorem ipsum" },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(product => 
+          <li key={product.id}>
+            <ProductItem
+              title={product.name}
+              price={product.price}
+              description={product.description}
+              id={product.id}
+            />
+          </li>
+        )}
       </ul>
     </section>
   );
