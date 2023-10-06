e90408b32e1923743674e8ed24b30dd4788e749b
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-06 08:29:49 +0200

    show user feedback for http request sending

--- 14-async-redux/src/App.js
+++ 14-async-redux/src/App.js
@@ -1,27 +1,80 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux/es/hooks/useSelector';
-import { useEffect } from 'react';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
 
 function App() {
-
-  const showCart = useSelector(state => state.ui.cartIsOpen);
-  const cart = useSelector(state => state.cart.items);
+  const showCart = useSelector((state) => state.ui.cartIsOpen);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    //sending http request
-    fetch("https://64ea398abf99bdcc8e676b68.mockapi.io/cart", {
-      method: 'PUT',
-      body: JSON.stringify(cart)
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.setNotificiation({
+          status: "pending",
+          title: "Sending...",
+          msg: "Sending cart data",
+        })
+      );
+      //sending http request
+      const response = await fetch(
+        "https://64ea398abf99bdcc8e676b68.mockapi.io/cart/1",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        dispatch(
+          uiActions.setNotificiation({
+            status: "error",
+            title: "Error",
+            msg: "Sending dta failed",
+          })
+        );
+        throw new Error("Sending data failed");
+      }
+      dispatch(
+        uiActions.setNotificiation({
+          status: "success",
+          title: "Success",
+          msg: "Cart data sent",
+        })
+      );
+    };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.setNotificiation({
+          status: "error",
+          title: error.name || "Error",
+          msg: error.message || "Error occured",
+        })
+      );
     });
-  }, [cart])
+    //no need to use useCallback as dispatch will never change
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart &&<Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.msg}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- 14-async-redux/src/store/ui-slice.js
+++ 14-async-redux/src/store/ui-slice.js
@@ -4,10 +4,15 @@ export const uiSlice = createSlice({
   name: "ui",
   initialState: {
     cartIsOpen: false,
+    notification: null,
   },
   reducers: {
     toggleCartIsOpen(state) {
       state.cartIsOpen = !state.cartIsOpen;
+    },
+    setNotificiation(state, action) {
+      const { status, title, msg } = action.payload;
+      state.notification = { status: status, title: title, mgs: msg };
     },
   },
 });
