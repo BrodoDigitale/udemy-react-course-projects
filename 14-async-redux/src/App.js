cd2ab46fb7f725f8395f0b861b0450909bca40dd
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-06 07:41:53 +0200

    add async code with useEffect

--- 14-async-redux/src/App.js
+++ 14-async-redux/src/App.js
@@ -1,12 +1,21 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-
 import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { useEffect } from 'react';
 
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsOpen);
+  const cart = useSelector(state => state.cart.items);
+
+  useEffect(() => {
+    //sending http request
+    fetch("https://64ea398abf99bdcc8e676b68.mockapi.io/cart", {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+  }, [cart])
 
   return (
     <Layout>
