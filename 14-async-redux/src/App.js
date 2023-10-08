9cd8ef8bcfed6309c7ddcc7ef396a83d1026a64e
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-08 21:34:12 +0200

    complete thunk function

--- 14-async-redux/src/App.js
+++ 14-async-redux/src/App.js
@@ -2,72 +2,12 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux/es/hooks/useSelector";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
-
-let isInitialRender = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsOpen);
-  const cart = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
   const notification = useSelector((state) => state.ui.notification);
 
-  useEffect(() => {
-
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.setNotificiation({
-          status: "pending",
-          title: "Sending...",
-          msg: "Sending cart data",
-        })
-      );
-      //sending http request
-      const response = await fetch(
-        "https://64ea398abf99bdcc8e676b68.mockapi.io/cart/1",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        dispatch(
-          uiActions.setNotificiation({
-            status: "error",
-            title: "Error",
-            msg: "Sending dta failed",
-          })
-        );
-        throw new Error("Sending data failed");
-      }
-      dispatch(
-        uiActions.setNotificiation({
-          status: "success",
-          title: "Success",
-          msg: "Cart data sent",
-        })
-      );
-    };
-
-    if(isInitialRender) {
-      isInitialRender = false;
-      return;
-    }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.setNotificiation({
-          status: "error",
-          title: error.name || "Error",
-          msg: error.message || "Error occured",
-        })
-      );
-    });
-    //no need to use useCallback as dispatch will never change
-  }, [cart, dispatch]);
 
   return (
     <>
--- 14-async-redux/src/store/cart-slice.js
+++ 14-async-redux/src/store/cart-slice.js
@@ -11,18 +11,17 @@ export const cartSlice = createSlice({
     addProductToCart(state, action) {
       state.totalQuantity++;
       const item = action.payload;
-      const existingItem = state.items.find(i => i.id === item.id);
-      if(existingItem) {
+      const existingItem = state.items.find((i) => i.id === item.id);
+      if (existingItem) {
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + item.price;
-
       } else {
         state.items.push({
-            id: item.id,
-            name: item.name,
-            price: item.price,
-            quantity: 1,
-            totalPrice: item.price
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          quantity: 1,
+          totalPrice: item.price,
         });
       }
     },
@@ -30,8 +29,8 @@ export const cartSlice = createSlice({
       state.totalQuantity--;
       const id = action.payload;
       const existingItem = state.items.find((i) => i.id === id);
-      if(existingItem.quantity === 1) {
-        state.items = state.items.filter(i => i.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((i) => i.id === id);
       } else {
         existingItem.quantity--;
         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
@@ -40,13 +39,47 @@ export const cartSlice = createSlice({
   },
 });
 
-export const sendCartData = () => {
-  //async code
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.setNotificiation({
+        status: "pending",
+        title: "Sending...",
+        msg: "Sending cart data",
+      })
+    );
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://64ea398abf99bdcc8e676b68.mockapi.io/cart/1",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        dispatch(
+          uiActions.setNotificiation({
+            status: "error",
+            title: "Error",
+            msg: "Sending dta failed",
+          })
+        );
+        throw new Error("Sending data failed");
+      }
+    };
 
-  //..fetching data
+    try {
+      await sendRequest();
+    } catch (error) {
+      dispatch(
+        uiActions.setNotificiation({
+          status: "error",
+          title: error.name || "Error",
+          msg: error.message || "Error occured",
+        })
+      );
+    }
 
-  //reducer we actually want to dispatch
-  return (dispatch) => {
     dispatch(
       uiActions.setNotificiation({
         status: "success",
@@ -54,9 +87,7 @@ export const sendCartData = () => {
         msg: "Cart data sent",
       })
     );
-
-  }
-
+  };
 };
 
 export const cartActions = cartSlice.actions;
