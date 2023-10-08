065ce38ac700df44a8fb39dad2a9cf57fee527ba
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-08 21:40:41 +0200

    dispatch thunk action from appjs

--- 14-async-redux/src/App.js
+++ 14-async-redux/src/App.js
@@ -2,12 +2,26 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux/es/hooks/useSelector";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { sendCartData } from "./store/cart-slice";
 import Notification from "./components/UI/Notification";
+
+let isInitialRender = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsOpen);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
   const notification = useSelector((state) => state.ui.notification);
 
+  useEffect(() => {
+    if(isInitialRender) {
+      isInitialRender = false;
+      return;
+    }
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
 
   return (
     <>
