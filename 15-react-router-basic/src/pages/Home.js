4981bb606963cbddf28e1e869ee68f9df54a333a
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-10 08:22:00 +0200

    add useNavigate hook to change routes programmatically

--- 15-react-router-basic/src/pages/Home.js
+++ 15-react-router-basic/src/pages/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
+    const navigate = useNavigate();
+
+    const navigateHandler = () => {
+        navigate("/products");
+    }
     return (
       <>
         <h1>This is my homepage</h1>
         <p>
           Go to <Link to="/products">products page</Link>
         </p>
+        <button onClick={navigateHandler}>Go to Products!</button>
       </>
     );
 }
