bf3805fe4451923fb7b28dffdd8fcbda98dcf72c
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-10 19:40:05 +0200

    links to dynamic routes

--- 15-react-router-basic/src/pages/Products.js
+++ 15-react-router-basic/src/pages/Products.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+const DUMMY_PRODUCTS = [
+  { id: "p1", title: "product1" },
+  { id: "p2", title: "product2" },
+  { id: "p3", title: "product3" },
+];
 export const Products = () => {
   return (
     <>
       <h1>This is my Products</h1>
       <ul>
-        <li>product 1</li>
-        <li>product 2</li>
-        <li>product 3</li>
+        {DUMMY_PRODUCTS.map((product) => {
+          return (
+            <li key={product.id}>
+              <Link to={`${product.title}`}>{product.title}</Link>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
