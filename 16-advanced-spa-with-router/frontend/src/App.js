187490ea76d8760ab58cedd45c68f8aed03c686d
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-12 08:49:50 +0200

    add another nested layout for events

--- 16-advanced-spa-with-router/frontend/src/App.js
+++ 16-advanced-spa-with-router/frontend/src/App.js
@@ -5,6 +5,7 @@ import { EventDetail } from "./pages/EventDetailPage";
 import { NewEventPage } from "./pages/NewEventPage";
 import { EditEventPage } from "./pages/EditEventPage";
 import { RootLayout } from "./pages/RootLayout";
+import { EventPageLayout } from "./pages/EventPageLayout";
 
   const router = createBrowserRouter([
     {
@@ -13,7 +14,16 @@ import { RootLayout } from "./pages/RootLayout";
       children: [
         { path: "/", element: <HomePage /> },
         { path: "/events", element: <Events /> },
-        { path: "/events/:id", element: <EventDetail /> },
+        {
+          path: "/events/:id",
+          element: <EventPageLayout />,
+          children: [
+            {
+              path: "/events/:id",
+              element: <EventDetail />,
+            },
+          ],
+        },
         { path: "/events/:id/edit", element: <EditEventPage /> },
         { path: "/events/new", element: <NewEventPage /> },
       ],
