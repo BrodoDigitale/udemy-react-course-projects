87045562406d5adc0d9ffc04a37127b9611fa30f
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-15 14:49:03 +0200

    add navigation loading state

--- 16-advanced-spa-with-router/frontend/src/pages/RootLayout.js
+++ 16-advanced-spa-with-router/frontend/src/pages/RootLayout.js
@@ -1,11 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { MainNavigation } from "../components/MainNavigation";
 
 export const RootLayout = () => {
+  const navigation = useNavigation();
+  const state = navigation.state // "idle" | "loading" | "submitting"
   return (
     <>
       <MainNavigation />
       <main>
+        {state === "loading" && <p>Loading...</p>}
         <Outlet />
       </main>
     </>
