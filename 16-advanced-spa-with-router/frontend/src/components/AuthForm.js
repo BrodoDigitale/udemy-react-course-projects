8a94df8e0b7c56174c90b093801d37416edaaf67
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-20 08:21:26 +0200

    add user feedback for login form

--- 16-advanced-spa-with-router/frontend/src/components/AuthForm.js
+++ 16-advanced-spa-with-router/frontend/src/components/AuthForm.js
@@ -1,16 +1,29 @@
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import { Form, Link, useActionData, useNavigation, useSearchParams } from "react-router-dom";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 function AuthForm() {
-
   const [searchParams] = useSearchParams();
-  const isLogin = searchParams.get('mode') === 'login';
+  const isLogin = searchParams.get("mode") === "login";
+
+  //response will return only in case of errors
+  const data = useActionData();
+
+  const { state } = useNavigation();
+  const isSubmitting = state === 'submitting';
 
   return (
     <>
       <Form method="post" className={classes.form}>
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
+        <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -20,10 +33,10 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
-            {isLogin ? 'Create new user' : 'Login'}
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+            {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>{isSubmitting ? 'Submitting...' : 'Save'}</button>
         </div>
       </Form>
     </>
