10691271017d7bf24425ac2170162cfe3d719951
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-21 13:45:44 +0200

    add automatic logout

--- 16-advanced-spa-with-router/frontend/src/pages/RootLayout.js
+++ 16-advanced-spa-with-router/frontend/src/pages/RootLayout.js
@@ -1,9 +1,20 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLoaderData, useNavigation, useSubmit } from "react-router-dom";
 import { MainNavigation } from "../components/MainNavigation";
+import { useEffect } from "react";
 
 export const RootLayout = () => {
   const navigation = useNavigation();
   const state = navigation.state // "idle" | "loading" | "submitting"
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if(token) {
+      setTimeout(() => {
+        submit(null, {action: "/logout", method: "post"});
+      }, 1 * 60 * 60 * 1000); //1 hour in milliseconds
+    }
+  }, [token, submit])
   return (
     <>
       <MainNavigation />
