837b3fb7e9dfbccae0db2c5322b95e5be0a720d0
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-25 08:19:47 +0200

    add more tests

--- 18-test-react/src/components/Greeting.js
+++ 18-test-react/src/components/Greeting.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
+
+
+
 export const Greeting = () => {
+    const [text, setText] = useState("Button");
+
+
+   const handleTextChange = () => {
+        if(text === "Button") {
+            setText("Text changed")
+        } else {
+            setText("Button")
+        }
+    }
+
   return (
     <div>
       <h2>Hello world</h2>
       <p>Good to see yo!</p>
+      <button onClick={handleTextChange}>{text}</button>
     </div>
   );
 };
--- 18-test-react/src/components/Greeting.test.js
+++ 18-test-react/src/components/Greeting.test.js
@@ -1,16 +1,25 @@
 import { Greeting } from "./Greeting";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"
 
 describe("greeting component", () => {
   test("renders hello world", () => {
-    //Arrange
     render(<Greeting />);
-
-    //Act
-    //...
-
-    //Assert
     const helloWorldEl = screen.getByText("Hello world", { exact: false });
     expect(helloWorldEl).toBeInTheDocument();
+  });
+
+  test("button NOT clicked", () => {
+    render(<Greeting />);
+    const button = screen.getByText("Button", { exact: true });
+    expect(button).toBeInTheDocument();
+  });
+
+  test("button clicked", () => {
+    render(<Greeting />);
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    const buttonWithNewText = screen.getByText("Text changed", { exact: true });
+    expect(buttonWithNewText).toBeInTheDocument();
   });
 });
