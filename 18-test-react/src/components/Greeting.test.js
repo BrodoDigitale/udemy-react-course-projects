439b4f7049c0b3330348280a2dbcfb90249df1db
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-25 07:40:55 +0200

    add test suite

--- 18-test-react/src/components/Greeting.test.js
+++ 18-test-react/src/components/Greeting.test.js
@@ -1,16 +1,16 @@
-import { Greeting } from "./Greeting"
-import { render, screen } from "@testing-library/react"
+import { Greeting } from "./Greeting";
+import { render, screen } from "@testing-library/react";
 
-test('renders hello world', () => {
-
+describe("greeting component", () => {
+  test("renders hello world", () => {
     //Arrange
-    render(<Greeting/>)
+    render(<Greeting />);
 
     //Act
     //...
 
     //Assert
-    const helloWorldEl = screen.getByText('Hello world', {exact: false});
+    const helloWorldEl = screen.getByText("Hello world", { exact: false });
     expect(helloWorldEl).toBeInTheDocument();
-
-})
+  });
+});
