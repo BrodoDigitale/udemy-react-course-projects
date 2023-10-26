44ae731d9e9edeb5a052f7558539923955e83a4e
Iuliia Sensi <sensi.julia@gmail.com>
2023-10-26 08:08:33 +0200

    add mock function

--- 18-test-react/src/components/Async.test.js
+++ 18-test-react/src/components/Async.test.js
@@ -1,15 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
-describe('Async component test', () => {
-    test('renders data if request succeeds', async () => {
-      //Arrange
-      render(<Async />);
-
-      //Act - no steps
-      //Assert
-
-      const listItems = await screen.findAllByRole("listitem", {}, 3000);
-      expect(listItems).not.toHaveLength(0);
+describe("Async component test", () => {
+  test("renders data if request succeeds", async () => {
+    //Arrange
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", name: "first post" }],
     });
-})
+    render(<Async />);
+
+    //Act - no steps
+    //Assert
+    const listItems = await screen.findAllByRole("listitem", {}, 3000);
+    expect(listItems).not.toHaveLength(0);
+  });
+});
