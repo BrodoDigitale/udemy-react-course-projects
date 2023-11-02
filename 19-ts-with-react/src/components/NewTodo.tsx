9961637a8cec87d0aeecfd6bde1336110a1682d6
Iuliia Sensi <sensi.julia@gmail.com>
2023-11-02 07:48:26 +0100

    add a ref

--- 19-ts-with-react/src/components/NewTodo.tsx
+++ 19-ts-with-react/src/components/NewTodo.tsx
@@ -1,13 +1,18 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
 export const NewTodo = () => {
+  const todoRefInput = useRef<HTMLInputElement>(null);
   const submitHandler = (evt: FormEvent) => {
     evt.preventDefault();
+    const enteredText = todoRefInput.current!.value;
+    if(enteredText.trim().length === 0) {
+        return;
+    }
   };
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="todo">Todo</label>
-      <input type="text" id="todo" />
+      <input ref={todoRefInput} type="text" id="todo" />
       <button>Add todo</button>
     </form>
   );
