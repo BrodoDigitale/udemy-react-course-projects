abf2062fa594e2ff7d84b82ccebd6e77421efbe8
Iuliia Sensi <sensi.julia@gmail.com>
2023-11-07 08:42:25 +0100

    create search query

--- 20-tanstack-query/src/components/Events/FindEventSection.jsx
+++ 20-tanstack-query/src/components/Events/FindEventSection.jsx
@@ -1,10 +1,40 @@
-import { useRef } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useRef, useState } from 'react';
+import { fetchEvents } from '../../util/http';
+import LoadingIndicator from '../UI/LoadingIndicator';
+import ErrorBlock from '../UI/ErrorBlock';
+import EventItem from '../Events/EventItem';
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [seachTerm, setSearchTerm] = useState('')
+
+  const { data, isPending, isError, error } = useQuery({
+    //by constructing a query key dynamically react query can cache(and reuse) different data
+    //for different keys based on the same query
+    queryKey:  ['events', {search: seachTerm}],
+    queryFn: () => {fetchEvents(seachTerm)}
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value)
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>
+
+  if(isPending) {
+    content = <LoadingIndicator />
+  }
+
+  if(isError) {
+    content = <ErrorBlock title="OOPS! An error occured" message={error.info?.message || "Failed to fetch"}/>
+  }
+
+  if(data) {
+    content = <li className='events-list'>
+      {data.map(event => <li key={event.id}><EventItem event={event}/></li>)}
+    </li>
   }
 
   return (
@@ -20,7 +50,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
