8bc2cabd1755e7fcde18f72da99932500cd0134d
Iuliia Sensi <sensi.julia@gmail.com>
2023-11-13 07:48:59 +0100

    add delete modal

--- 20-tanstack-query/src/components/Events/EventDetails.jsx
+++ 20-tanstack-query/src/components/Events/EventDetails.jsx
@@ -1,20 +1,23 @@
+import { useState } from "react";
 import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 import Header from "../Header.jsx";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
+import Modal from "../UI/Modal.jsx";
 
 export default function EventDetails() {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
 
-  const { mutate, isPending } = useMutation({
+  const { mutate, isPending, isError: isDeleteError, error: deleteError } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
       queryClient.invalidateQueries({
@@ -28,6 +31,14 @@ export default function EventDetails() {
   const handleDelete = () => {
     mutate({ id: id });
   };
+
+  const startDeleting = () => {
+    setIsDeleting(true);
+  }
+
+  const stopDeleting = () => {
+    setIsDeleting(false)
+  }
 
   let content;
 
@@ -53,10 +64,20 @@ export default function EventDetails() {
     });
     content = (
       <>
+       {isDeleting && <Modal onClose={stopDeleting}>
+        <h2>Are you sure?</h2>
+        <p>Delete this item permanetly?</p>
+        {isPending && <LoadingIndicator/>}
+        {!isPending && <div className="form-actions">
+          <button className="button-text" onClick={stopDeleting}>Close</button>
+          <button className="button" onClick={handleDelete}>Delete</button>
+        </div>}
+        {isDeleteError && <ErrorBlock title={'Failed to delete'} message={deleteError.info?.message || "Error ocurred"}/>}
+       </Modal>}
         <header>
           <h1>{title}</h1>
           <nav>
-            <button onClick={handleDelete}>Delete</button>
+            <button onClick={startDeleting}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
