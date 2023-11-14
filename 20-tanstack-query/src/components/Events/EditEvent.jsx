63f8231daafd329731a780ec766a4b08fecfa265
Iuliia Sensi <sensi.julia@gmail.com>
2023-11-14 08:04:45 +0100

    load data with react router and react query

--- 20-tanstack-query/src/components/Events/EditEvent.jsx
+++ 20-tanstack-query/src/components/Events/EditEvent.jsx
@@ -1,51 +1,49 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { fetchEvent, updateEvent, queryClient } from "../../util/http.js";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { useMutation } from "@tanstack/react-query";
 
 export default function EditEvent() {
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const { data, isLoading, isError, error } = useQuery({
+  const { data, isError, error } = useQuery({
     queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
 
-    const { mutate } = useMutation({
+  const { mutate } = useMutation({
     mutationFn: updateEvent,
     onMutate: async (data) => {
       const newEvent = data.event;
-      
-      //stores original data using the query key
-      const previousEventData = queryClient.getQueryData(['events', id]);
 
-      await queryClient.cancelQueries({queryKey: ['events', id]})
-      queryClient.setQueryData(['events', id], newEvent);
+      //stores original data using the query key
+      const previousEventData = queryClient.getQueryData(["events", id]);
+
+      await queryClient.cancelQueries({ queryKey: ["events", id] });
+      queryClient.setQueryData(["events", id], newEvent);
 
       //return value to make availiable in the context in case of error
       return {
-        previousEvent: previousEventData
-      }
+        previousEvent: previousEventData,
+      };
     },
     //on error roll back will be done
     onError: (error, data, context) => {
-      queryClient.setQueryData(['events', id], content.previousEvent);
+      queryClient.setQueryData(["events", id], content.previousEvent);
     },
     //will be called when mutation finishes no matter with which result
     //is important to align be with fe
     onSettled: () => {
-      queryClient.invalidateQueries(['events', id]);
-    }
+      queryClient.invalidateQueries(["events", id]);
+    },
   });
 
   function handleSubmit(formData) {
-    mutate( {id, event: formData})
+    mutate({ id, event: formData });
     navigate("../");
   }
 
@@ -54,10 +52,6 @@ export default function EditEvent() {
   }
 
   let content;
-
-  if (isLoading) {
-    content = <LoadingIndicator />;
-  }
 
   if (isError) {
     content = (
@@ -86,3 +80,12 @@ export default function EditEvent() {
 
   return <Modal onClose={handleClose}>{content}</Modal>;
 }
+
+//there is no need to remove useQuery from thw component!
+//useQuery will use cached data
+export const loader = ({params}) => {
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  });
+};
